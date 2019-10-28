5c9907e6a5329595b00f88e65eb54201dda2b414
Justin Armstrong <jxa1762@rit.edu>
2019-10-28 01:22:39 -0400

    DomoMaker B finished

---
+++ server/app.js
@@ -0,0 +1,54 @@
+// - REQUIRE -
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+// Part B
+const session = require('express-session');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+
+// setting up mongoose
+mongoose.connect(dbURL, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+// pull from routes
+const router = require('./router.js');
+
+// set up express app
+const app = express();
+
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(session({
+  key: 'sessionid',  // the name of the cookie so that it can be tracked when requests are made
+  secret: 'Domo Arigato',  // string used as a seed for hashing/creating unique session keys
+  resave: true,  // refresh the key to keep it active
+  saveUninitialized: true  // make sessions even when not loggin in
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', '{__dirname}/../views');
+app.use(cookieParser());
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
